00be4bfc1b198d5b3c1c3c66d195d8e4b12d9b3f
SIAM-AHMED-CP <118670054+SIAM-AHMED-CP@users.noreply.github.com>
2023-01-15 19:36:20 +0600

    Add files via upload

---
+++ JXB.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+import os, sys, platform
+#os.system('xdg-open https://facebook.com/groups/302474258349320/')
+os.system('git pull')
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import x18
+    x18.run()
+ 
+elif bit == '32bit':
+    import x1832
+    x1832.run()
